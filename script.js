8cd2402f52eb4822cbe03250850ab85f4a8dd565
Juan <juanillomoralesrodriguez@gmail.com>
2026-04-13 18:55:25 +0200

    Removed page for contact and updated numbers and reviews

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     const header = document.querySelector('.header');
     window.addEventListener('scroll', function() {
-        if (window.scrollY > 100/*edit to makke shadow appear sooner or later*/) {
+        if (window.scrollY > 1000/*edit to makke shadow appear sooner or later*/) {
             header.classList.add('scrolled');
         } else {
             header.classList.remove('scrolled');
